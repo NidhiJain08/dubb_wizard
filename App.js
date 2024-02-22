e0c883bac0103b4ce6c8b4877b799ee99fbca0d1
Nikki <ikitayadav@gmail.com>
2024-02-22 17:25:16 +0530

    created form.ejs

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-
 const app = express();
+
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/views/form', (req,res)=>{
+    res.render("form")
+})
+
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000 ')
